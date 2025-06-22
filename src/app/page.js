f47b17dce1919c0c16eba8311ae049f0bfd5d2c9
Roni Taboada <roni@MacBook-Air-de-Roni.local>
2025-06-22 09:04:07 -0300

    New Project component

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutSection";
+import ProjectSection from "./components/ProjectSection";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <div className="container mt-24 px-12 py-8">
         <Hero />
         <AboutSection />
+        <ProjectSection />
       </div>
     </main>
   );
